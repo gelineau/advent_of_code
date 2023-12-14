88abb871bae4cca34e7cbcbd207d66cfd5b7d3c3
Francois Gelineau <francois.gelineau@orange.com>
2023-12-14 10:30:02 +0100

    Update utils

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
+from pathlib import Path
 from typing import Iterator, Any
 from parse import compile
 
 
-def read_lines_from_input(filename) -> Iterator[str]:
+def read_lines_from_input(filename: str) -> Iterator[str]:
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             yield line.strip()
 
 
-def read_words_from_input(filename) -> Iterator[list[str]]:
+def read_words_from_input(filename: str) -> Iterator[list[str]]:
     for line in read_lines_from_input(filename):
         yield line.split()
 
@@ -24,3 +25,11 @@ def read_dicts_from_input(
         if result is None:
             raise ValueError(f"could not parse `{line}` with template `{template}`")
         yield result.named
+
+def read_text_from_input(filename: str) -> str:
+    return Path(filename).read_text()
+
+def read_line_groups_from_input(filename: str) -> Iterator[list[str]]:
+    groups = read_text_from_input(filename).split("\n\n")
+    for group in groups:
+        yield group.split("\n")
